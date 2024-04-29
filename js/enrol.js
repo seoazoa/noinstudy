6d194d8248c35ae6e0d667f12a8bb4cfafd2fd4a
suminkim <di05023@gmail.com>
2024-04-29 17:31:47 +0900

    B 서브 페이지 추가

--- js/enrol.js
+++ js/enrol.js
@@ -33,28 +33,44 @@ window.addEventListener("load", function () {
     const dataInit = document.getElementById("data-info");
     let html = "";
 
-    if (storedData.length === 3) {
-      const areaValue = storedData[0].areaValue || "";
-      const centerValue = storedData[1].centerValue || "";
-      const dataValue = storedData[2].dataValue || "";
+    // 센터와 강의를 저장할 변수 초기화
+    let latestCenter = "";
+    let latestEnrol = "";
 
-      // 신청한 지역과 강의 정보를 한 번만 출력
+    // 최신 데이터가 존재하는 경우에만 처리
+    if (storedData.length > 0) {
+      // 모든 데이터를 반복하면서 센터와 강의에 대한 최신 값을 찾음
+      storedData.forEach((item) => {
+        if (item.centerValue) {
+          latestCenter = item.centerValue;
+        }
+        if (item.dataValue) {
+          latestEnrol = item.dataValue;
+        }
+      });
+
+      // 센터와 강의에 대한 최신 값을 출력
+      if (latestCenter) {
+        html += `
+          <li class="enrol-info-li">신청하신 지역은 <b>${latestCenter}</b> 입니다.</li>
+        `;
+      }
+      if (latestEnrol) {
+        html += `
+          <li class="enrol-info-li">희망하신 강의는 <b>${latestEnrol}</b> 입니다.</li>
+        `;
+      }
+
+      // "강의 지역 및 장소 / 강의 재선택하기" 링크 추가
       html += `
-      <li class="enrol-info-li">신청하신 지역은 <b>${areaValue}, ${centerValue}</b> 입니다.<br>
-      희망하신 강의는 <b>${dataValue}</b> 입니다.</li>
-      <li class="sub-link">
-        <a href="edu_list.html">강의 지역 및 장소 / 강의 재선택하기</a>
-      </li>
-    `;
+        <li class="sub-link">
+          <a href="edu_list.html">강의 지역 및 장소 / 강의 재선택하기</a>
+        </li>
+      `;
     }
 
     dataInit.innerHTML = html;
   } else {
     console.log("저장된 데이터가 없습니다.");
   }
-  // "강의 지역 및 장소 / 강의 재선택하기" 링크를 클릭할 때의 이벤트 리스너
-  document.querySelector(".sub-link a").addEventListener("click", function (event) {
-    // 로컬 스토리지에서 저장된 데이터 초기화
-    localStorage.removeItem("clickedValues");
-  });
 });
